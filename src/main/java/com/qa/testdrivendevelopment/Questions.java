22bec8bbcf45f195fbbf83ef07b8514786967696
nPaloukosQA <62283168+nPaloukosQA@users.noreply.github.com>
2020-04-03 09:36:09 +0300

    first commit

---
+++ src/main/java/com/qa/testdrivendevelopment/Questions.java
@@ -0,0 +1,30 @@
+package com.qa.testdrivendevelopment;
+
+public class Questions {
+	/**
+	 * There is a well known mnemonic which goes "I before E, except after C", which is used to determine which
+	 * order "ei" or "ie" should be in a word.  <br>
+	 *  <br>
+	 * Write a function which returns the boolean True if a string follows the mnemonic, and returns the
+	 * boolean False if not.  <br>
+	 *  <br>
+	 * For Example:  <br>
+	 * iBeforeE("ceiling") → True  <br>
+	 * iBeforeE("believe") → True  <br>
+	 * iBeforeE("glacier") → False  <br>
+	 * iBeforeE("height") → False
+	 */
+	public boolean iBeforeE(String input) {
+		input = input.toLowerCase();
+		if (input.contains("cei")) {
+			return true;
+		}
+		if (input.contains("ie")) {
+			if (input.contains("cie")) {
+				return false;
+			}x
+			return true;
+		}
+		return false;
+	}
+}
